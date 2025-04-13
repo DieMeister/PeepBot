03dd7a03a1a6c171549befa02567fcab90800e67
DieMeister <juliusmeister@gmx.net>
2025-04-13 18:04:24 +0200

    fixes spelling error in Cog module

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 This is going to be a fun little discord bot create together with the community of the Sinaheh discord server
 # Before self-hosting
 - replace `BOTTOKEN` with your BotToken at the end of `main.py`
-- 
 ## Dependencies
 - discord.py
--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import os
 
 class PeepBot(commands.Bot):
 	async def setup_hook(self) -> None:
-		for file in os.listdir("./cogs"):
+		for file in os.listdir("./Cogs"):
 			if file.endswith(".py"):
-				await self.load_extension(f"cogs.{file[:-3]}")
+				await self.load_extension(f"Cogs.{file[:-3]}")
 
 
 intents = discord.Intents.all()
