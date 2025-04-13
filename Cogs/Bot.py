f899824ab198bbc999aefeb57af998848fe797f6
DieMeister <juliusmeister@gmx.net>
2025-04-13 18:05:01 +0200

    adds function reload_cog()

--- Cogs/Bot.py
+++ Cogs/Bot.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord.ext.commands import ExtensionFailed, ExtensionNotLoaded, ExtensionNotFound, NoEntryPointError
 
 
 class Bot(commands.Cog):
@@ -6,9 +7,17 @@ class Bot(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def sync(self, ctx) -> None:
+    async def sync(self, ctx) -> None:  # TODO check if user is allowed to execute command
         await self.bot.tree.sync()
         await ctx.send("Commands synced")
+
+    @commands.command()
+    async def reload_cog(self, ctx, cog: str) -> None:  # TODO check if user is allowed to execute command
+        try:
+            await self.bot.reload_extension(f"Cogs.{cog}")
+            await ctx.send("Cog reloaded successfully")
+        except (ExtensionFailed, ExtensionNotLoaded, ExtensionNotFound, NoEntryPointError):
+            await ctx.send("Cog could not be reloaded")  # TODO differentiate between errors
 
 
 async def setup(bot) -> None:
