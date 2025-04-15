36b334abeabe90aed7bb9c10455c343af689500c
DieMeister <juliusmeister@gmx.net>
2025-04-16 01:09:05 +0200

    calls the logging fuction when neccessary

--- Cogs/Bot.py
+++ Cogs/Bot.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 from discord.ext.commands import ExtensionFailed, ExtensionNotLoaded, ExtensionNotFound, NoEntryPointError
 
+import logic
+
 
 class Bot(commands.Cog):
     def __init__(self, bot) -> None:
@@ -9,14 +11,17 @@ class Bot(commands.Cog):
     @commands.command()
     async def sync(self, ctx) -> None:  # TODO check if user is allowed to execute command
         await self.bot.tree.sync()
+        logic.log("info", "bot", "Commands synced")
         await ctx.send("Commands synced")
 
     @commands.command()
     async def reload_cog(self, ctx, cog: str) -> None:  # TODO check if user is allowed to execute command
         try:
             await self.bot.reload_extension(f"Cogs.{cog}")
+            logic.log("info", "bot", "Cog reloaded")
             await ctx.send("Cog reloaded successfully")
         except (ExtensionFailed, ExtensionNotLoaded, ExtensionNotFound, NoEntryPointError):
+            logic.log("warning", "bot", "Cog failed to reload")
             await ctx.send("Cog could not be reloaded")  # TODO differentiate between errors
 
 
--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import discord
 from discord.ext import commands
 import os
 
+import tokens
+import logic
+
 
 class PeepBot(commands.Bot):
 	async def setup_hook(self) -> None:
 		for file in os.listdir("./Cogs"):
 			if file.endswith(".py"):
 				await self.load_extension(f"Cogs.{file[:-3]}")
+		logic.log("info", "main", "cogs loaded")
 
 
 intents = discord.Intents.all()
 bot = PeepBot(command_prefix="!", intents=intents)
 
-bot.run(BOTTOKEN)  # FIXME create BotToken
+bot.run(tokens.butter_test)  # FIXME create BotToken
