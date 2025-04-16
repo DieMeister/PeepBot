7b548f7edb1eb684138e88b88edd15f6edf6e436
DieMeister <juliusmeister@gmx.net>
2025-04-17 01:00:40 +0200

    adds patreon social link command

--- Cogs/TwitchCommands.py
+++ Cogs/TwitchCommands.py
@@ -10,7 +10,16 @@ class TwitchCommands(commands.Cog):
     @commands.command()
     async def ao3(self, ctx) -> None:  # TODO check who is allowed to use the command
         await ctx.reply("check out sina's ao3 [here](https://archiveofourown.org/users/sinaheh/profile):")
-        logic.logging("info", "twchcmds", "ao3 link sent", {
+        logic.logging("info", "twchcmds", "ao3 social link sent", {
+            "guild": ctx.guild.id,
+            "channel": ctx.channel.id,
+            "user": ctx.author.id
+        })
+
+    @commands.command()
+    async def patreon(self, ctx) -> None:  # TODO check who is allowed to use the command
+        await ctx.reply("BECOME A PATREON MEMBER (to gain early access to the [WEBTOON](https://www.webtoons.com/en/canvas/way-of-the-living-weapon/list?title_no=993451), step-by-step art tutorials, WIPs & lot more extra content) ([patreon link](https://patreon.com/sinaheh))")
+        logic.logging("info", "twchcmds", "patreon social link sent", {
             "guild": ctx.guild.id,
             "channel": ctx.channel.id,
             "user": ctx.author.id
