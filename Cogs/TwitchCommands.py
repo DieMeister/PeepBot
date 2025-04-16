008a8582a5c4cb885f6c4dc1b5ad45b27011ba09
DieMeister <juliusmeister@gmx.net>
2025-04-17 01:06:11 +0200

    adds pinterest social link command

--- Cogs/TwitchCommands.py
+++ Cogs/TwitchCommands.py
@@ -8,7 +8,7 @@ class TwitchCommands(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def ao3(self, ctx) -> None:  # TODO check who is allowed to use the command
+    async def ao3(self, ctx) -> None:  # TODO check who is allowed to use this command
         await ctx.reply("check out sina's ao3 [here](https://archiveofourown.org/users/sinaheh/profile):")
         logic.logging("info", "twchcmds", "ao3 social link sent", {
             "guild": ctx.guild.id,
@@ -17,9 +17,18 @@ class TwitchCommands(commands.Cog):
         })
 
     @commands.command()
-    async def patreon(self, ctx) -> None:  # TODO check who is allowed to use the command
+    async def patreon(self, ctx) -> None:  # TODO check who is allowed to use this command
         await ctx.reply("BECOME A PATREON MEMBER (to gain early access to the [WEBTOON](https://www.webtoons.com/en/canvas/way-of-the-living-weapon/list?title_no=993451), step-by-step art tutorials, WIPs & lot more extra content) ([patreon link](https://patreon.com/sinaheh))")
         logic.logging("info", "twchcmds", "patreon social link sent", {
+            "guild": ctx.guild.id,
+            "channel": ctx.channel.id,
+            "user": ctx.author.id
+        })
+
+    @commands.command()
+    async def pinterest(self, ctx) -> None:  # TODO check who is allowed to use this command
+        await ctx.reply("follow sina on [pinterest](https://www.pinterest.com/sinaheh/)!")
+        logic.logging("info", "twchcmds", "pinterest social link sent", {
             "guild": ctx.guild.id,
             "channel": ctx.channel.id,
             "user": ctx.author.id
