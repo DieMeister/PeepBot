35d6def2abe0774872224e3c60a5dac4a3d83f42
DieMeister <juliusmeister@gmx.net>
2025-04-16 03:50:31 +0200

    removes file in front of exception name when raised

--- errors.py
+++ errors.py
@@ -1,8 +1,10 @@
 class LoggingTypeError(Exception):
     """ Inappropriate logging type """
+    __module__ = "builtins"
     pass
 
 
 class LoggingModuleError(Exception):
     """ Inappropriate logging module """
+    __module__ = "builtins"
     pass
