81bbb26cc3f423f7a7160933dcf30d3467654f77
DieMeister <juliusmeister@gmx.net>
2025-04-18 23:13:35 +0200

    shortens type annotations

--- Cogs/TwitchCommands.py
+++ Cogs/TwitchCommands.py
@@ -2,7 +2,12 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
+from typing import TYPE_CHECKING
+
 import logic
+
+if TYPE_CHECKING:
+    from discord.ext.commands import Context
 
 
 class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
@@ -73,7 +78,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
 
 
     @commands.command()
-    async def ao3(self, ctx) -> None:
+    async def ao3(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("check out sina's ao3 [here](https://archiveofourown.org/users/sinaheh/profile):")
             logic.logging("info", "twchcmds", "ao3 social link sent", {
@@ -83,7 +88,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
             })
 
     @commands.command()
-    async def patreon(self, ctx: discord.ext.commands.Context) -> None:
+    async def patreon(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("BECOME A PATREON MEMBER (to gain early access to the [WEBTOON](https://www.webtoons.com/en/canvas/way-of-the-living-weapon/list?title_no=993451), step-by-step art tutorials, WIPs & lot more extra content) ([patreon link](https://patreon.com/sinaheh))")
             logic.logging("info", "twchcmds", "Patreon social link sent", {
@@ -103,7 +108,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
             })
 
     @commands.command()
-    async def prints(self, ctx: discord.ext.commands.Context) -> None:
+    async def prints(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("[PRINT SHOP](https://www.inprnt.com/gallery/sinaheh/) IS NOW OPEN")
             logic.logging("info", "twchcmds", "prints social link sent", {
@@ -113,7 +118,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
             })
 
     @commands.command()
-    async def socials(self, ctx: discord.ext.commands.Context) -> None:
+    async def socials(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("FOLLOW SINA'S SOCIALS! [twitter](https://www.twitter.com/sinaheh), [main insta](https://www.instagram.com/sinahehlive/), [art insta](https://www.instagram.com/sinahehart/), [tiktok](https://www.tiktok.com/@sinaheh), [tumblr](https://www.tumblr.com/sinaheh), [bluesky](https://bsky.app/profile/sinaheh.bsky.social), [youtube](https://www.youtube.com/@sinaheh)")
             logic.logging("info", "twchcmds", "all social links sent", {
@@ -123,7 +128,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
             })
 
     @commands.command()
-    async def spotify(self, ctx: discord.ext.commands.Context) -> None:
+    async def spotify(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("[here](https://open.spotify.com/user/sinaxdd/playlists)'s all sina's playlists")
             logic.logging("info", "twchcmds", "Spotify social link sent", {
@@ -133,7 +138,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
             })
 
     @commands.command()
-    async def tiktok(self, ctx: discord.ext.commands.Context) -> None:
+    async def tiktok(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("follow sina on [tiktok](https://www.tiktok.com/@sinaheh)!")
             logic.logging("info", "twchcmds", "TikTok social link sent", {
@@ -143,7 +148,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
             })
 
     @commands.command()
-    async def twitter(self, ctx: discord.ext.commands.Context) -> None:
+    async def twitter(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("follow sina on twitter! [main](https://twitter.com/sinaheh), [alt](https://twitter.com/sinaltheh)")
             logic.logging("info", "twchcmds", "Twitter social link sent", {
@@ -153,7 +158,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
             })
 
     @commands.command()
-    async def wishlist(self, ctx: discord.ext.commands.Context) -> None:
+    async def wishlist(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("consider buying sina a [gift](https://thronegifts.com/u/sinaheh):")
             logic.logging("info", "twchcmds", "wishlist social link sent", {
@@ -163,7 +168,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
             })
 
     @commands.command()
-    async def art(self, ctx: discord.ext.commands.Context) -> None:
+    async def art(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("sina uses Clip Studio Paint program and HUION Kamvas 22 Plus to draw! ☆ current GO TO brushes can be found [here](https://sinahehbrushes.carrd.co)")
             logic.logging("info", "twchcmds", "art faq sent", {
@@ -175,7 +180,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
     # TODO birthday
 
     @commands.command()
-    async def brush(self, ctx: discord.ext.commands.Context) -> None:
+    async def brush(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("find all sina's GO TO brushes [here](https://sinahehbrushes.carrd.co)")
             logic.logging("info", "twchcmds", "brush faq sent", {
@@ -185,7 +190,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
             })
 
     @commands.command()
-    async def comms(self, ctx: discord.ext.commands.Context) -> None:
+    async def comms(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("SINA'S [COMMISSIONS](https://drive.google.com/file/d/1x0Wh0QQyXxagA0EZ3NRDwVZSuCyS9r9j/view) ARE OPEN!! if interested you can contact her through instagram DMs or email")
             logic.logging("info", "twchcmds", "commission faq sent", {
@@ -195,7 +200,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
             })
 
     @commands.command()
-    async def ocs(self, ctx: discord.ext.commands.Context) -> None:
+    async def ocs(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("you can read all about sina's original characters and their lore [here](https://sinahehocs.carrd.co)")
             logic.logging("info", "twchcmds", "original character faq sent", {
@@ -205,7 +210,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
             })
 
     @commands.command()
-    async def ocmusic(self, ctx: discord.ext.commands.Context) -> None:
+    async def ocmusic(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("[LEO'S PLAYLIST](https://open.spotify.com/playlist/0KqlAQ1niYOZCByeyoGnP3?si=0149eb8b07024d8f) // [LORIÉN'S PLAYLIST](https://open.spotify.com/playlist/63voqKGasjy8cTAHYBlB4V?si=7fc4f096a3b84924) // [KALEO PLAYLIST](https://open.spotify.com/playlist/5xPg2jGce4P49oM13r21Pz?si=0ee05ae61ca44c4e)")
             logic.logging("info", "twchcmds", "character playlist faq sent", {
@@ -215,7 +220,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
             })
 
     @commands.command()
-    async def pronouns(self, ctx: discord.ext.commands.Context) -> None:
+    async def pronouns(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("sina goes by **she/they**! thank you for asking!!")
             logic.logging("info", "twchcmds", "pronouns faq sent", {
@@ -227,7 +232,7 @@ class TwitchCommands(commands.Cog):  # TODO make the messages embeds?
     # TODO time/timezone
 
     @commands.command()
-    async def webtoon(self, ctx: discord.ext.commands.Context) -> None:
+    async def webtoon(self, ctx: "Context") -> None:
         if logic.has_property(logic.data["execute_twitch_commands"], ctx.author.roles):
             await ctx.reply("WAY OF THE LIVING WEAPON IS NOW AVAILABLE ON [WEBTOON](https://www.webtoons.com/en/canvas/way-of-the-living-weapon/list?title_no=993451) AND [TAPAS](https://tapas.io/series/Way-of-the-Living-Weapon/info) ☆ you can get early access to new chapters through my [patreon](https://patreon.com/sinaheh)!")
             logic.logging("info", "twchcmds", "webtoon faq sent", {
